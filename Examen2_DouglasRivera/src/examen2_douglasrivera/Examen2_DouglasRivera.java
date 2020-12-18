ee4af58c84b8d88c9de1a54ecd29b7e9e8e6ab40
DouglasRivera <riveradouglas19@gmail.com>
2020-12-18 13:23:39 -0600

    Comienzo para el concurso

---
+++ Examen2_DouglasRivera/src/examen2_douglasrivera/Examen2_DouglasRivera.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen2_douglasrivera;
+
+/**
+ *
+ * @author urs1
+ */
+public class Examen2_DouglasRivera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
